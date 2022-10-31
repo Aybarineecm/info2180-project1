95fda1a68eec66aad2eb3bd5d5d218d03e74e623
Aybarineecm <aybarineecampbellmendez@gmail.com>
2022-10-30 23:10:17 -0500

    added v3

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-/* Add your JavaScript to this file */
-window.addEventListener('load', function(){
+
+window.addEventListener('load', function()
+{
     var submit = document.querySelector(".newsletter button");
-    var message = document.querySelector(".message");
+    var feedback = document.querySelector(".message");
     const email = document.getElementById("email");
     var reGex = /^\S+@\S+\.\S+$/i;
 
@@ -12,11 +13,18 @@ window.addEventListener('load', function(){
         event.preventDefault();
         var userEmail = email.value;
 
-        if(userEmail === "" || !reGex.test(userEmail)) {
+        if(userEmail === "" || !reGex.test(userEmail))
+        
+        {
 
-            message.textContent = "Please enter a valid email address";
-        }else{
-            message.textContent = `Thank You! Your email address ${userEmail} has been added to our mailing list!`;
+            feedback.textContent = "Please enter a valid email address";
+        }
+        
+        else
+        
+        {
+
+            feedback.textContent = `Thank You! Your email address ${userEmail} has been added to our mailing list!`;
         }
     });
 });
